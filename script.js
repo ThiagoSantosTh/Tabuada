aa99d77ce2bbdb902bc8b8e8a4ad1536ea3f5c4a
Thiago Santos <61941921+ThiagoSantosTh@users.noreply.github.com>
2021-04-16 11:31:32 -0300

    Tabuada - JavaScript

---
+++ script.js
@@ -0,0 +1,19 @@
+function tabuada(){
+    var numero = document.getElementById('txtn')
+    var tabuada = document.getElementById('seltab')
+
+    if(numero.value.length == 0){
+        alert('Por favor, digite um número!')
+    }else{
+        var n = Number(numero.value)
+        var c = 1
+        tabuada.innerHTML = ''
+        while(c <= 10){
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tabuada.appendChild(item)
+            c++
+        }
+    }
+    
+}
